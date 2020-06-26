5be23ebd6c049ac41791e22495a210c041cecace
Jordan Walker <jwalker117@gmail.com>
2020-06-26 17:10:08 -0500

    uploading project from ztm with comments on redux

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+//For the search action
+export const CHANGE_SEARCH_FIELD = 'CHANGE_SEARCH_FIELD';
+
+//For the get action for robots
+export const REQUEST_ROBOTS_PENDING = 'REQUEST_ROBOTS_PENDING';
+export const REQUEST_ROBOTS_SUCCESS = 'REQUEST_ROBOTS_SUCCESS';
+export const REQUEST_ROBOTS_FAILED = 'REQUEST_ROBOTS_FAILED';
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//Needed to create the redux store
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+//Need to install redux logger to monitor changes(middleware)
+import {createLogger} from 'redux-logger';
+//Need to install redux-thunk for async functions I think they are like sagas
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import {searchRobots, requestRobots} from './reducer';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Create a root reducer to hold all your reducers to be used with createStore
+const rootReducer = combineReducers({searchRobots, requestRobots});
+//Enable middleware for redux logger
+const logger = createLogger();
+//Create the Redux store
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
